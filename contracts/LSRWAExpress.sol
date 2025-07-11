6f173b010820a5a6aebdfbb7f7802396763097a9
Alexander Mogavero <alexander.mogavero128@gmail.com>
2025-07-12 01:08:16 +0930

    add ownership

--- contracts/LSRWAExpress.sol
+++ contracts/LSRWAExpress.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract LSRWAExpress {
+contract LSRWAExpress is Ownable {
     using SafeERC20 for IERC20;
     // --- Tokens ---
     IERC20 public immutable usdc;
@@ -93,7 +94,7 @@ contract LSRWAExpress {
         _;
     }
 
-    constructor(address _usdc, address _lsrwa) {
+    constructor(address _usdc, address _lsrwa) Ownable(msg.sender) {
         admin = msg.sender;
         usdc = IERC20(_usdc);
         lsrwa = IERC20(_lsrwa);
